5bd07cf10edfbfa9d47cbe97c2f9e52940f5de98
julius george <106617931+juliusgeorge12@users.noreply.github.com>
2022-10-06 18:38:18 +0100

    Create start_development_server.bat

--- start_development_server.bat
+++ start_development_server.bat
@@ -2,7 +2,6 @@
  title  development server setup
  echo welcome 
  set /A default_port = 8080
-  :prompt
   setlocal EnableDelayedExpansion
   set /p port= "enter the port on which you want to start the development server: "
   ::check if the user entered anything if no use the default port else use the port entered
